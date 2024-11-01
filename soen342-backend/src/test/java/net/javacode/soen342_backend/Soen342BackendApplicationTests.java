f6c1ee59b54ce6c0e4ca456c120e926cb4f1d772
Mohamed <msanoko5@gmail.com>
2024-11-01 19:32:53 -0400

    spring boot starter code with additional dependencies

--- soen342-backend/src/test/java/net/javacode/soen342_backend/Soen342BackendApplicationTests.java
+++ soen342-backend/src/test/java/net/javacode/soen342_backend/Soen342BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.SOEN342Project;
+package net.javacode.soen342_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Soen342ProjectApplicationTests {
+class Soen342BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
