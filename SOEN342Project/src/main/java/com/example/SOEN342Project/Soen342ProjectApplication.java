480bc5cabc8298e8c8bc7c1fcbef02464c8c14a6
Mohamed <msanoko5@gmail.com>
2024-11-01 18:58:21 -0400

    adding spring boot initial code

---
+++ SOEN342Project/src/main/java/com/example/SOEN342Project/Soen342ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SOEN342Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soen342ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Soen342ProjectApplication.class, args);
+	}
+
+}
