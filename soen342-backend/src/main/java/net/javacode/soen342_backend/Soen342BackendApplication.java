6a11754ebc84d5a393a1273506dd271dee47497e
Mohamed Sanoko <130871457+mohamedsanoko@users.noreply.github.com>
2024-11-05 18:06:10 -0500

    adding spring boot repo with required dependencies (#29)
    * Added SSD_ProcessBooking.drawio
    
    * Update SSD_ProcessBooking.drawio
    
    * Process Bookings SSD Snapshot
    
    * Adding operation contract for Process Booking Use Case
    
    * Added SS_initializeBooking.drawio
    
    * Delete SS_initializeBooking.drawio
    
    * Added SS_searchAvailability.drawio
    
    * Update SS_searchAvailability.drawio
    
    * Added SS_selectOffering.drawio
    
    * Update SS_selectOffering.drawio
    
    * Added SS_enterCustomerDetails.drawio
    
    * Update SS_enterCustomerDetails.drawio
    
    * Added SS_addGuardian.drawio
    
    * Update SS_addGuardian.drawio
    
    * Added SS_viewBookingDetails.drawio
    
    * Update SS_viewBookingDetails.drawio
    
    * Added SS_cancelBooking.drawio
    
    * Update SS_cancelBooking.drawio
    
    * Adding code folder
    
    * Removing folder
    
    * adding spring boot initial code
    
    * spring boot starter code with additional dependencies
    
    * Delete SOEN342Project directory

--- soen342-backend/src/main/java/net/javacode/soen342_backend/Soen342BackendApplication.java
+++ soen342-backend/src/main/java/net/javacode/soen342_backend/Soen342BackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.SOEN342Project;
+package net.javacode.soen342_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Soen342ProjectApplication {
+public class Soen342BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Soen342ProjectApplication.class, args);
+		SpringApplication.run(Soen342BackendApplication.class, args);
 	}
 
 }
