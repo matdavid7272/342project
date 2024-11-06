26bfdb983a39302614c1c5babce53374d3f71784
David <matdavid7272@gmail.com>
2024-11-05 19:25:48 -0500

    frontend added

--- soen342-backend/src/test/java/net/javacode/soen342_backend/Soen342BackendApplicationTests.java
+++ soen342-backend/src/test/java/net/javacode/soen342_backend/Soen342BackendApplicationTests.java
@@ -1,13 +1,13 @@
-package net.javacode.soen342_backend;
+// package net.javacode.soen342_backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Soen342BackendApplicationTests {
+// @SpringBootTest
+// class Soen342BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
